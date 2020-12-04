84112fbbe4c2ae31c0e00497af615e5ba4440ad8
Alejandro Vera <alejandroverah97@gmail.com>
2020-12-03 19:57:20 -0500

    capa modelo con conexion

---
+++ model/conectar.php
@@ -0,0 +1,27 @@
+<?php
+
+class Conectar {
+
+    //clase estática
+    public static function conexion() {
+
+        try {
+            $conexion=new PDO ('mysql:host=localhost; dbname=curso_sql', 'root', '');
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conexion->exec("SET CHARACTER SET UTF8");
+        
+        }
+        catch (Exception $e) {
+            die("Error" . $e->getMessage());
+            echo "Linea del error" . $e->getLine();
+        }
+        return $conexion;
+
+    }
+}
+
+
+
+
+
+?>
---
+++ model/productos_model.php
@@ -0,0 +1,44 @@
+<?php
+
+class Productos_modelo{
+
+//variables encapsuldas
+private $db;
+private $productos;
+
+public function __construct() {
+
+    //conectar con BBDD
+    require_once("conectar.php");
+
+    //especificar la variable donde quiero almacenar la conexion que nos va a devolver el metodo
+    $this->db=conectar::conexion();
+
+    //definiendo la variable productos como array
+    $this->productos=array();
+}
+
+public function get_productos(){
+
+    $consulta=$this->db->query("SELECT * FROM PRODUCTOS");
+
+    //FETCH ASSOC para leer un array asociativo
+    while($filas=$consulta->fetch(PDO::FETCH_ASSOC)){
+
+        //recorre las filas del array consultas
+        $this->productos[]=$filas;
+    }
+
+    return $this->productos;
+
+}
+
+}
+
+
+
+
+
+
+
+?>
